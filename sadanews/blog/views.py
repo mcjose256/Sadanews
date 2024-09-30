7920a64193256dfad2010d6600f61535e75b4529
Akandwanaho Joseph <jakandwanaho2@gmail.com>
2024-09-30 19:31:31 +0300

    added settings

--- sadanews/blog/views.py
+++ sadanews/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def post_list(request):
-    return render(request,'blog/postlist.html')
+    return render(request,'blog/post_list.html')
     
 #def room(request):
     #return render(request,"room.html")
